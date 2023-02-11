2320ba1ecf091a1eb4ad6c216e479abfb35e31be
Devansh <91583975+Devansh-bharadwaj@users.noreply.github.com>
2023-02-11 17:20:39 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,27 +1,40 @@
 import Header from "./components/header/Header";
-import Home from "./components/herosection/home/Home";
+// import Home from "./components/herosection/home/Home";
 import Footer from "./components/footer/Footer";
-import './App.css';
+import "./App.css";
 import About from "./components/herosection/about/About";
 import Resume from "./components/herosection/resume/Resume";
 import Portfolio from "./components/herosection/portfolio/Portfolio";
-import Skills from "./components/herosection/resume/Skills";
+// import Skills from "./components/herosection/resume/Skills";
 import Contact from "./components/herosection/contact/Contact";
+import React, { Suspense, lazy } from "react";
+import Preloader from "./components/preloader/Preloader";
 
+const AsyncHome = lazy(() => import("./components/herosection/home/Home"));
+const AsyncSkills = lazy(() =>
+  import("./components/herosection/resume/Skills")
+);
+const AsyncResume = lazy(() => import("./components/herosection/resume/Resume"));
+const AsyncPortfolio = lazy(() => import("./components/herosection/portfolio/Portfolio"));
+const AsyncAbout = lazy(() => import("./components/herosection/about/About"));
+const AsyncContact = lazy(() => import("./components/herosection/contact/Contact"));
+const AsyncFooter = lazy(() => import("./components/footer/Footer"));
 
 function App() {
   return (
     <>
-    <div className="wrapper">
-    <Header />
-    <Home />
-    <Skills />
-    <Resume/>
-    <Portfolio />
-    <About />
-    <Contact />
-    <Footer />
-    </div>
+      <Suspense fallback={<Preloader />}>
+        <div className="wrapper">
+          <Header />
+          <AsyncHome />
+          <AsyncSkills />
+          <AsyncResume />
+          <AsyncPortfolio />
+          <AsyncAbout />
+          <AsyncContact />
+          <AsyncFooter />
+        </div>
+      </Suspense>
     </>
   );
 }
